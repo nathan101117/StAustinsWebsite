de8d51c570cc2930dc8002586e8f164d98d00cae
nathan101117 <124667960+nathan101117@users.noreply.github.com>
2025-04-09 01:42:38 +0300

    fix: moved nuxt to dependencies for Vercel build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
-  }
+  },
+
+  compatibilityDate: '2025-04-09'
 })
